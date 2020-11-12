9db04f1734d3d98e6af6499fcda58b6c883a49e0
lik173 <70771696+lik173@users.noreply.github.com>
2020-11-12 10:41:43 +0300

    Add files via upload

---
+++ README.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Just tool to start some apps and other 
+echo With creating repository i create new changes! 
+echo Im add RU language 
+echo Now in Utils section have a problem.Sorry. 
+echo You can contact with me: contact.lik173@mail.ru 
+echo You can modify this files 
+echo Share some batch scripts to me (not DDoS please)
+pause
+set /p yn=Want to see repository on GitHub?(y/n)
+
+if %yn%==y start iexplore.exe https://github.com/lik173/Batch_Commander
+if %yn%==n goto end
+
+:end
